49fb71458f09e31e9ab66bdf24d6c1c8a2bfb3d5
Justus Schwabedal <jschwabedal@gmail.com>
2017-05-02 14:48:47 +0200

    Added explicit args and kwargs, refact. kernel.
    - kwargs names were refactored to better reflect their meaning. The
      docstring was updated.
    - the trajectory kernel was optimized for speed and readability.

--- pyedr/synthetic.py
+++ pyedr/synthetic.py
@@ -4,17 +4,18 @@ from numpy.random import normal
 from namedlist import namedlist
 from collections import namedtuple, ChainMap
 
-pi2 = 2*np.pi
+pi  = np.pi
+pi2 = 2*pi
 
 __all__ = ['SyntheticECG']
 
-def get_respiratory_phase(num_samples, sampling_rate, frequency=15.0/60.0, stdev=1.0/60.0):
+def get_respiratory_phase(num_samples, sampling_rate, frequency=15.0/60.0, stdev_factor=0.05):
     """ 
     Returns:
         array[num_samples]: the phase (as func of time)
     """
     w  = pi2 * frequency
-    dw = pi2 * stdev
+    dw = w * stdev_factor
     dt = 1/np.float64(sampling_rate)
     sqdt = np.sqrt(dt)
     t = dt * np.arange(num_samples)
@@ -34,39 +35,32 @@ class SyntheticECGGenerator:
         IEEE TRANSACTIONS ON BIOMEDICAL ENGINEERING, VOL. 50, NO. 3, MARCH 2003
 
     Args:
-        max_time: length of sequence in sec (default 10)
         sampling_rate: samples per second (default 250)
+        num_samples:  number of samples to generate on call (default 5000)
         heart_rate: heartrate in beats per second  (default 60/60)
-        respiration_rate: ensemble average of the respiration rate
-            in beats per second (default 15/60)
-        respiration_rate_stdev: the standard deviation of the
-            respiration rate (default 1/60) for both the ensemble fluctuations
-            and the insample fluctuations
-        esk_strength: ESK coupling stength, the ecg signal varies with
+        hr_stdev_factor: fraction of 'heart_rate' as its variability (default 0.05)
+        respiration_rate: respiration rate in beats per second (default 15/60)
+        rr_stdev_factor: fraction of 'respiration_rate' as its variability (default 0.2)
+        EKG_noise_strength: standard deviation of additive noise (default 0.05)
+        EKG_fluctuation_strength: stdev factor for variability of EKG waves (default 1)
+        RESP_noise_strength: stdev of additive noise added to respiration signal (default 0.1)
+        esk_strength: ESK coupling stength, the EKG signal varies with
             1 + esk_strength * respiration   (default 0.1)
-        rsa_strength: strength of the respiratory sinus arrhythmia (default 0.5)
-        heart_noise_stength: noise added to ecg signal (default 0.05)
-        heart_rate_fluctuations: relative heat rate fluctuations (default 0.05)
-        respiration_noise_stength: noise added to respiration signal
-            (default 0.05)
+        rsa_strength: strength of the respiratory sinus arrhythmia (default 1.0)
+        rsa_dispersion: slope of sensitivity increase at the RSA-sensitive part in the EKG (default 0.1).
+        rsa_width_shift: width of the RSA-sensitive part in the EKG (default 0.0).
+        seed:  random seed to be passed to numpy.random.seed (default None)
+
+    kwargs:
+        Additional parameters can modify the WaveParameter for 'P', 'Q', 'R', 'S', and 'T' waves.
+        It should have the form '<Wave>_<parameter>=<value>'.
+        parameter:
+        - a: Amplitude of the wave
+        - b: Half width of the peak in radian.
+        - theta: Phase of the peak in radian.
+        - esk: electrostatic coupling of the peak to RESP.
+        - da, db, dtheta: standard deviation of peak-to-peak variability of above parameters.
     """
-    defaults = {
-        'sampling_rate': 250,
-        'heart_noise_strength': 0.05,
-        'heart_fluctuation_strength': 1,  # is relative
-        'respiration_noise_strength': 0.05,
-        'heart_rate_fluctuations': 0.1,
-        'heart_rate': 60.0/60.0,
-        'respiration_rate': 15.0/60.0,
-        'respiration_rate_stdev': 1/60,
-        'respiration_fluctuation': 1/60,
-        'esk_strength': 0.1,
-        'rsa_strength': 0.5,
-        'rsa_width_shift': 0.0,
-        'rsa_dispersion': 0.1,
-        'num_samples': 1024,
-        'seed': None
-    }
 
     Signal = namedtuple("SyntheticEKG", ["input", "target"])
 
@@ -74,27 +68,70 @@ class SyntheticECGGenerator:
         "Parameter", ["a", "b", "theta", "esk", 
                       "da", "db", "dtheta"])
     WAVE_PARAMETERS = {
-        "P": WaveParameter(a= .20, b=.25, theta=-np.pi/3,  esk= .0,
-                           da=0.01, db=0.02, dtheta=0),
-        "Q": WaveParameter(a=-.20, b=.1,  theta=-np.pi/12, esk= .0,
-                           da=0.01, db=0.02, dtheta=0),
-        "R": WaveParameter(a=3.00, b=.1,  theta=0,         esk= .2,
-                           da=.10, db=0.02, dtheta=0),
-        "S": WaveParameter(a=-.35, b=.1,  theta=np.pi/12,  esk=-.1,
-                           da=0.01, db=0.02, dtheta=0),
-        "T": WaveParameter(a= .55, b=.3,  theta=np.pi/2,   esk= .0,
-                           da=0.01, db=0.02, dtheta=0)
+        "P": WaveParameter(a= .25, b=pi2*.04,  theta=-pi/3,  esk= .5, da=0.05, db=pi2*0.002, dtheta=pi2*0.03),
+        "Q": WaveParameter(a=-.20, b=pi2*.01,  theta=-pi/12, esk=-.5, da=0.02, db=pi2*0.001, dtheta=pi2*0.03),
+        "R": WaveParameter(a=2.20, b=pi2*.015, theta=0,      esk= .5, da=.15,  db=pi2*0.002, dtheta=pi2*0.03),
+        "S": WaveParameter(a=-.15, b=pi2*.01,  theta=pi/12,  esk=-.5, da=0.02, db=pi2*0.001, dtheta=pi2*0.03),
+        "T": WaveParameter(a= .60, b=pi2*.06,  theta=pi/1.7, esk= .5, da=0.1,  db=pi2*0.002, dtheta=pi2*0.03)
     }
 
-    def __init__(self, **kwargs):
-        self.__dict__.update(**ChainMap(kwargs, self.defaults))
+    def __init__(self,
+            sampling_rate=250,
+            num_samples=5000,
+            heart_rate=60.0/60.0,
+            hr_stdev_factor=0.03,
+            respiration_rate=15.0/60.0,
+            rr_stdev_factor=0.2,
+            EKG_noise_strength=0.05,
+            EKG_fluctuation_strength=0.2,
+            RESP_noise_strength=0.1,
+            esk_strength=0.1,
+            rsa_strength=1.0,
+            rsa_dispersion=0.1,
+            rsa_width_shift=0.0,
+            seed=None,
+            **kwargs):
+        self.sampling_rate = sampling_rate
+        self._hr_stdev_factor = hr_stdev_factor
+        self.heart_rate = heart_rate
+        self.respiration_rate = respiration_rate
+        self.rr_stdev_factor = rr_stdev_factor
+        self.EKG_noise_strength = EKG_noise_strength
+        self.EKG_fluctuation_strength = EKG_fluctuation_strength
+        self.RESP_noise_strength = RESP_noise_strength
+        self.esk_strength = esk_strength
+        self.rsa_strength = rsa_strength
+        self.rsa_width_shift = rsa_width_shift
+        self.rsa_dispersion = rsa_dispersion
+        self.num_samples = num_samples
+        self.seed = seed
+
         np.random.seed(self.seed)
-        self.omega_heart_mean = pi2 * self.heart_rate
         for k, v in kwargs.items():
-            w_tuple = k.split('_')
-            if w_tuple[0] in self.WAVE_PARAMETERS:
-                wname, pname = w_tuple
+            wp_tuple = k.split('_')
+            if wp_tuple[0] in self.WAVE_PARAMETERS:
+                wname, pname = wp_tuple
                 self.set_wave_param(wname, pname, v)
+
+    @property
+    def heart_rate(self):
+        return self._heart_rate
+
+    @heart_rate.setter
+    def heart_rate(self, hr):
+        self._heart_rate = hr
+        self.w_heart  = pi2 * hr
+        self.dw_heart = self.w_heart * self.hr_stdev_factor
+
+    @property
+    def hr_stdev_factor(self):
+        return self._hr_stdev_factor
+
+    @hr_stdev_factor.setter
+    def hr_stdev_factor(self, dhr):
+        self._hr_stdev_factor = dhr
+        self.dw_heart = self.w_heart * dhr
+
 
     def set_wave_param(self, wave_name, param_name, val):
         setattr(self.WAVE_PARAMETERS[wave_name], param_name, val)
@@ -113,7 +150,7 @@ class SyntheticECGGenerator:
             Q(tht, R) = strength R(t) / (1+exp((cos(tht)+shift)/width))
         """
         Q = self.rsa_strength/(1+np.exp((np.cos(theta)+self.rsa_width_shift)/self.rsa_dispersion)) * resp_state
-        return self.omega_heart_mean + Q
+        return self.w_heart + Q
 
     def EKG_from_phase(self, phase, RESP=None):
         """Computes EKG from a heartbeat phase timeseries
@@ -130,7 +167,7 @@ class SyntheticECGGenerator:
         # Local namespace is sometimes faster, often better readable
         esk_strg  = self.esk_strength
         wavep     = self.WAVE_PARAMETERS
-        fluc_strg = self.heart_fluctuation_strength
+        fluc_strg = self.EKG_fluctuation_strength
         EKG = np.zeros_like(phase, dtype=np.float64)
         for peak_idx in range(int(min(phase) / pi2) - 10, int(max(phase) / pi2) + 10):
             for a_i, b_i, tht_i, esk, da, db, dtheta in iter(wavep.values()):
@@ -155,29 +192,28 @@ class SyntheticECGGenerator:
         if show: plt.show()
 
     def get_resp_phase(self, num_samples):
-        return get_respiratory_phase(num_samples, self.sampling_rate, self.respiration_rate, self.respiration_rate_stdev)
+        return get_respiratory_phase(num_samples, self.sampling_rate, self.respiration_rate, self.rr_stdev_factor)
 
     def heartbeat_trajectory(self):
-        dt    = 1./np.float64(self.sampling_rate)
-        deriv = self.phase_deriv
-        num_int = self.num_samples
-        initial_state = 2.0*np.pi*np.random.rand()
-        resp_phase = self.get_resp_phase(num_int)
-        resp_states = np.cos(resp_phase)
-        heart_phase = np.zeros((num_int), np.float64)
-        heart_phase[0] = np.array(initial_state)
-        for n in range(1, num_int):
-            heart_phase[n] = heart_phase[n-1] + dt * deriv(heart_phase[n-1], resp_states[n-1])
-        EKG  = self.EKG_from_phase(heart_phase, resp_states)
-        trajectory = np.transpose(np.vstack((heart_phase, EKG, resp_states)))
+        dt = 1./np.float64(self.sampling_rate)
+        f = self.phase_deriv
+        N = self.num_samples
+        R = np.cos(self.get_resp_phase(N))
+        dW = self.dw_heart * np.sqrt(dt) * np.random.randn(N)
+        x = np.zeros((N), np.float64)
+        x[0] = pi2*np.random.rand()
+        for n in range(1, N):
+            x[n] = x[n-1] + dt * f(x[n-1], R[n-1]) + dW[n]
+        EKG  = self.EKG_from_phase(x, R)
+        trajectory = np.transpose(np.vstack((x, EKG, R)))
         return trajectory
 
     def __call__(self):
         heartbeat_trajectory = self.heartbeat_trajectory()
         EKG  = heartbeat_trajectory[:, 1]
         RESP = heartbeat_trajectory[:, 2]
-        EKG  += normal(0.0, self.heart_noise_strength,       size=EKG.size)
-        RESP += normal(0.0, self.respiration_noise_strength, size=RESP.size)
+        EKG  += normal(0.0, self.EKG_noise_strength,  size=EKG.size)
+        RESP += normal(0.0, self.RESP_noise_strength, size=RESP.size)
         return self.Signal(input=EKG, target=RESP)
 
 
